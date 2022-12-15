e8c2e0c2b5655994f7346f2ec2bf6fb8d12c688d
Ahmet Türkmen <f.ahmet.turkmen@icloud.com>
2022-12-15 02:57:27 +0100

    working orbis automation tool

--- orbis-access.py
+++ orbis-access.py
@@ -30,7 +30,6 @@ class Orbis:
         self.driver = webdriver.Chrome(executable_path=self.executable_path, chrome_options=self.chrome_options)
     
     def __exit__(self, exc_type, exc_value, traceback):
-        
         return self.driver.close()
         
     def __enter__(self):
@@ -62,9 +61,10 @@ class Orbis:
 
     def logout(self):
         # logout from the web site
+        time.sleep(3)
         self.driver.get(self.orbis_logout_url)
-        logout_button = self.driver.find_element(By.XPATH,"/html/body/section[2]/div[1]/div/div/div[1]/input")
-        logout_button.click()
+        
+        
         
         
     def read_xlxs_file(self, file_path):
@@ -74,37 +74,50 @@ class Orbis:
     def batch_search(self, file_path):
         
         self.driver.get(self.orbis_batch_search_url)
-        time.sleep(2)
+        # time.sleep(2)
         # WebDriverWait(self.driver, 20).until(EC.text_to_be_present_in_element((By.XPATH, "/html/body/section[3]/div[3]/div/div[2]/form/div/div/label/a"), 'View results'))
+        WebDriverWait(self.driver, 30*60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/section[3]/div[3]/div/div[2]/form/div/div/label/a")))
         self.driver.find_element(By.XPATH, "/html/body/section[3]/div[3]/div/div[2]/form/div/div/label/a").click()
-        time.sleep(2)
+        # time.sleep(2)
         file_input=self.driver.find_element(By.XPATH, "/html/body/section[3]/div[3]/div/div[2]/div/div/form/div[1]/div[1]/input[1]")
-        file_path = "/Users/mrturkmen/Desktop/idp-works/data/data.csv"
         file_input.send_keys(file_path)
-        time.sleep(2)
+        WebDriverWait(self.driver, 30*60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/section[3]/div[3]/div/div[2]/div/div/form/div[2]/p/a[2]")))
 
         self.driver.find_element(By.XPATH, "/html/body/section[3]/div[3]/div/div[2]/div/div/form/div[2]/p/a[2]").click()
-        # upload_field = form.find_element(By.CSS_SELECTOR,'input[name="id"]')
-        time.sleep(4)
+    
+        WebDriverWait(self.driver, 30*60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/section[2]/div[3]/div/form/div[1]/table/tbody/tr[2]/td[1]/input")))
         self.driver.find_element(By.XPATH, "/html/body/section[2]/div[3]/div/form/div[1]/table/tbody/tr[2]/td[1]/input").clear()
         time.sleep(2)
+
         self.driver.find_element(By.XPATH, "/html/body/section[2]/div[3]/div/form/div[1]/table/tbody/tr[2]/td[1]/input").send_keys(";")
+
         self.driver.find_element(By.XPATH, "/html/body/section[2]/div[3]/div/form/div[1]/table/tbody/tr[2]/td[1]/input").send_keys(Keys.RETURN)
+        
         print("field seperator is cleared")
         time.sleep(2)
+        WebDriverWait(self.driver, 30*60).until(EC.element_to_be_clickable((By.XPATH, '/html/body/section[2]/div[3]/div/form/div[3]/div[2]/input')))
+
         self.driver.find_element(By.XPATH, "/html/body/section[2]/div[3]/div/form/div[3]/div[2]/input").click()
+        
         time.sleep(5)
+        WebDriverWait(self.driver, 30*60).until(EC.invisibility_of_element_located((By.XPATH, '/html/body/section[2]/div[3]/div/form/div[1]/div[1]/div[1]')))
+
         
-                # '//*[@id="batch-search-toolbar"]/ul/li[1]/a' >> view results button
-        WebDriverWait(self.driver, 30*60).until(EC.text_to_be_present_in_element((By.XPATH, '/html/body/section[2]/div[1]/div[2]/ul/li[1]/a'), 'View results'))
+        warning_message = self.driver.find_element(By.XPATH, '/html/body/section[2]/div[3]/div/form/div[1]/div[1]/div[1]')
+        while warning_message.text == "Search is not finished":
+                time.sleep(5)
+                warning_message = self.driver.find_element(By.XPATH, '/html/body/section[2]/div[3]/div/form/div[1]/div[1]/div[1]')
+                print(warning_message.text)
         
         
-        view_result_sub_url = self.driver.find_element(By.XPATH, '/html/body/section[2]/div[1]/div[2]/ul/li[1]/a').get_attribute('href') 
+        WebDriverWait(self.driver, 30*60).until(EC.element_to_be_clickable((By.XPATH, '/html/body/section[2]/div[1]/div[2]/ul/li[1]')))
         
-        print(view_result_sub_url)
-        # self.driver.find_element(By.XPATH, '/html/body/section[2]/div[1]/div[2]/ul/li[1]/a').click()
-        self.driver.get(view_result_sub_url)
         
+        
+        
+        view_result_sub_url = self.driver.find_element(By.XPATH, '/html/body/section[2]/div[1]/div[2]/ul/li[1]/a')
+        view_result_sub_url.send_keys(Keys.RETURN)
+       
         
         WebDriverWait(self.driver, 30*60).until(EC.text_to_be_present_in_element((By.XPATH, '/html/body/section[2]/div[1]/div[2]/div[2]/div[2]/ul/li[3]/a'), 'Excel'))
         self.driver.find_element(By.XPATH, "/html/body/section[2]/div[1]/div[2]/div[2]/div[2]/ul/li[3]/a").click()
@@ -118,11 +131,18 @@ class Orbis:
         
         # # //*[@id="exportDialogForm"]/div[2]/a[2] >> download button from popup window
         # self.driver.find_element(By.XPATH, '//*[@id="exportDialogForm"]/div[2]/a[2]').click()
-        WebDriverWait(self.driver, 30*60).until(EC.text_to_be_present_in_element((By.XPATH, '/html/body/section[2]/div[6]/div[3]/a'), 'Download'))
+        WebDriverWait(self.driver, 30*60).until(EC.element_to_be_clickable((By.XPATH, '/html/body/section[2]/div[6]/div[3]/a')))
+
         # /html/body/section[2]/div[6]/div[3]/a >> download button from popup window
-        self.driver.find_element(By.XPATH, '/html/body/section[2]/div[6]/div[3]/a').click()
-         
-        time.sleep(10)
+        
+        while True:
+            download_button= self.driver.find_element(By.XPATH, '/html/body/section[2]/div[6]/div[3]/a')
+            if download_button.value_of_css_property("background-color") == "rgba(0, 20, 137, 1)":
+                download_button.send_keys(Keys.RETURN)                
+                break
+            else: 
+                time.sleep(2)
+       
         # time.sleep(180*10)
 
 
