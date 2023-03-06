c7df7b8ed2892637d1b2a174cffa57b2f955324c
Ahmet Türkmen <hi@mrturkmen.com>
2023-03-06 15:09:04 +0000

    pass service instead of executable

--- orbi/orbi.py
+++ orbi/orbi.py
@@ -136,12 +136,10 @@ class Orbis:
         if not self.offline:
             logger.debug("Starting chrome driver...")
             self.chrome_options = webdriver.ChromeOptions()
-            preferences = {"download.default_directory": self.data_dir ,
-                   "download.prompt_for_download": False,
-                   "directory_upgrade": True,
-                   "safebrowsing.enabled": True}
+            preferences = {"download.default_directory": self.data_dir}
 
             # add user agent to avoid bot detection
+            self.chrome_options.add_experimental_option("prefs", preferences)
             self.chrome_options.add_argument(self.headers)
             if environ.get("LOCAL_DEV") != "True":
                 self.chrome_options.add_argument("--headless")
@@ -151,9 +149,10 @@ class Orbis:
             self.chrome_options.add_argument("--window-size=1920,1080") 
             self.chrome_options.add_argument("--disable-gpu")   
             self.chrome_options.add_experimental_option("detach", True)
-            self.chrome_options.add_experimental_option("prefs", preferences)
+            chrome_service = ChromeService(self.executable_path)
+
             self.driver = webdriver.Chrome(
-                executable_path=self.executable_path, options=self.chrome_options)
+                service=chrome_service, options=self.chrome_options)
             logger.debug("Chrome driver started")
             # self.slack_client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
             time.sleep(1)
